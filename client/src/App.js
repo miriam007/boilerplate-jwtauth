cd82f0301184093179333326399486342bfd0276
Miriam <miriamu88@gmail.com>
2019-05-16 15:20:49 -0500

    nav bar added to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div>
-
+        <Navbar />
+    
       </div>
     );
   }
