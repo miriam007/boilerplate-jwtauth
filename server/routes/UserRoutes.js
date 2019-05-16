7bbc6e69e24f0f7b80a6c58a3c825a5ae5d5e80d
Miriam <miriamu88@gmail.com>
2019-05-16 14:30:13 -0500

    backend with auth for user login and register

---
+++ server/routes/UserRoutes.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
+const User = require('../models/UserModel');
+
+router.post('/register', function(req, res) {
+   
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if(user) {
+            return res.status(400).json({
+                email: 'Email is already taken.'
+            });
+        } 
+        else {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                pasword: req.body.password,
+                avatar
+            });
+            
+            bcrypt.genSalt(10, (err, salt) => {
+                if(err) console.error('There was an error', err);
+            else {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) console.error('There was an error', err);
+                    else {
+                        newUser.pasword = hash;
+                        newUser
+                            .save()
+                            .then(user => {
+                                res.json(user)
+                            });
+                    }
+                });
+            }            
+         });
+        }
+    });
+});
+
+router.post('./login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+        .then(user => {
+            if(!user) {
+                errors.email = 'User not found.'
+                return res.status(404).json(errors);
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        const payload ={
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar
+                        }
+                        jwt.sign(payload, 'secret', {
+                            expiresIn: 3600
+                        }, (err, token) => {
+                            if (err) console.error('There is some error in token.', err);
+                            else {
+                                res.json({
+                                    success: true,
+                                    token: `Bearer ${token}`
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        errors.password = 'Incorrect password.';
+                        return res.status(400).json(errors);
+                    }
+                });
+        });
+});
+
+router.get('/me', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+});
+
+module.exports = router;
