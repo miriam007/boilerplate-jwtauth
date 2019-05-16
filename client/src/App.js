90c95b2d638967e23d16e64490b1db41dd61fbe4
Miriam <miriamu88@gmail.com>
2019-05-16 15:57:08 -0500

    Register frontend added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
-import './App.css';
+import Register from './components/Register';
+// import Login from './components/Login';
+// import Home from './componentsHome';
 
 class App extends Component {
   render(){
     return (
-      <div>
-        <Navbar />
-    
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          {/* <Route exact path = "/" component = { Home } /> */}
+          <div className="container">
+            <Route exact path = "/register" component = { Register } />
+            {/* <Route exact path = "/login" component = { Login} /> */}
+          </div>
+        </div>
+      </Router>
     );
   }
 }
