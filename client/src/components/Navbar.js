a0ebf2f42093cbc23f9dfd4aa4713926c657bc59
Miriam <miriamu88@gmail.com>
2019-05-16 21:37:58 -0500

    updated navbar with classname

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
     render(){
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link class="navbar-brand" to="/">Redux Auth</Link>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/">Home</Link>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">Redux Auth</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/register">Register</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <Link class="nav-link" to="login">Login</Link>
                         </li>
                     </ul>
